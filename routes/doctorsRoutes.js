490cbc4afa12ff153b8d2f51b993e14aef4bbd9c
mateus <mateuslaalmeida@gmail.com>
2021-07-05 23:34:44 -0300

    feat: created post/create operation

--- routes/doctorsRoutes.js
+++ routes/doctorsRoutes.js
@@ -6,5 +6,6 @@ const doctorsController = require('../controllers/doctorsController');
 
 router.get('/doctor', doctorsController.getAllDoctors);
 router.get('/doctor/:id', doctorsController.getDoctorById);
+router.post('/doctor', doctorsController.createDoctor);
 
 module.exports = router;
